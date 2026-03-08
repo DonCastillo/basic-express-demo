1c28ed828da7901850c403a978769fc4e439fc15
DonCastillo <don.qcastillo@yahoo.com>
2026-03-08 19:53:41 -0400

    finished lab exercise

--- routes/users.js
+++ routes/users.js
@@ -23,16 +23,38 @@ let users = [
     },
 ];
 
+// Function to convert a date string in the format "dd-mm-yyyy" to a Date object
+function getDateFromString(strDate) {
+    let [dd, mm, yyyy] = strDate.split('-');
+    return new Date(yyyy + "/" + mm + "/" + dd);
+}
+
 // GET request: Retrieve all users
 router.get("/", (req, res) => {
     res.send(JSON.stringify({users}, null, 4));
 });
 
-// GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email", (req, res) => {
     const email = req.params.email;
     let filtered_users = users.filter((user) => user.email === email);
     res.send(filtered_users)//This line is to be replaced with actual return value
+});
+
+router.get("/lastName/:lastName", (req, res) => {
+    const lastName = req.params.lastName;
+    let filtered_lastname = users.filter((user) => user.lastName === lastName);
+    res.send(filtered_lastname);
+});
+
+
+// Define a route handler for GET requests to the "/sort" endpoint
+router.get("/sort", (req, res) => {
+    let sorted_users = users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+        return d1 - d2;
+    });
+    res.send(sorted_users);
 });
 
 
